f9e2edba985ff330cba6dcca0d3f0c1b2e122e0f
Pankaj Kumar <805.bluebell@gmail.com>
2019-09-16 11:03:16 +0530

    testing why fromDate toDate is ot working

--- __init__.py
+++ __init__.py
@@ -481,6 +481,9 @@ def generateReferalDict(fromDate, toDate):
 	except:
 		fromDate = datetime.datetime(2000,1,1)
 		toDate = datetime.datetime(2030,1,1)
+
+	print(f"{fromDate} ---------- {toDate}")
+	
 	query = {"Origin":"referred", "Current Stage":"New applicant", "Created At (GMT)":{"$gte":fromDate}, "Created At (GMT)":{"$lte":toDate}}
 	# proj = {'_id':0, 'Profile ID':1, 'Candidate Name':1, 'Application ID':1, 'Posting ID':1, 'Posting Title':1, 'Created At (GMT)':1}
 	rows = collection.find(query, cursor_type=CursorType.EXHAUST)
