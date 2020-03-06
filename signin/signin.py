a50a6aae07d700f32dc9a069384f3d5da07f69d5
805bluebell <805.bluebell@gmail.com>
2020-03-06 19:03:34 +0530

    spelling mistake

--- signin/signin.py
+++ signin/signin.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 
-module1_blueprint = Blueprint('module1', __name__,
+signin_blueprint = Blueprint('signin', __name__,
                         template_folder='templates',
                         static_folder='static')
-@module1_blueprint.route("/signin")
+@signin_blueprint.route("/signin")
 def signin():
     return render_template("signin/index.html")
     # return "module 1 churning"
