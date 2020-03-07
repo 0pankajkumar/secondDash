c0cc7f7edb21060a1e42c45d8547e773200d9881
805bluebell <805.bluebell@gmail.com>
2020-03-07 13:31:20 +0530

    Only build is there from nowonwards serving static & templates

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates',
-                    static_folder='static')
+app = Flask(__name__, template_folder='build',
+                    static_folder='build')
 
 @app.route("/signin")
 def signin():
