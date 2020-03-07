9d8dd77e32ec1da00373096dff82bfdbac0189b0
805bluebell <805.bluebell@gmail.com>
2020-03-07 13:46:19 +0530

    build added

---
+++ client/build/precache-manifest.78469cc9f979624b147d240b31b6b58e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b60e25d9b925de0f077d255a3e366c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0789c9965784bc6845eb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8a6aa80bb60e69203040",
+    "url": "/static/js/2.0ca2f169.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/static/js/2.0ca2f169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0789c9965784bc6845eb",
+    "url": "/static/js/main.61a6d33b.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
