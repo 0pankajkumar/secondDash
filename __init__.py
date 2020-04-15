cd9b30415c9140dcace9a303a0532471a26b0467
805bluebell <805.bluebell@gmail.com>
2020-04-15 18:14:57 +0530

    debugging 4

--- __init__.py
+++ __init__.py
@@ -1649,15 +1649,15 @@ def fetchUsers(usersList):
 			print("Hai to, phir kyu nahi dikh raha")
 		else:
 			print("Kuch to gadbad hai daya")
-		usersDict = dict()
-		usersDict['users'] = p['users']
-		usersDict['type'] = p['type']
-		usersDict['tatMember'] = p['tatMember']
-		if 'whichPositions' in p:
-			usersDict['whichPositions'] = p['whichPositions']
-		else:
-			usersDict['whichPositions'] = "Not defined"
-		usersList.append(usersDict)
+		# usersDict = dict()
+		# usersDict['users'] = p['users']
+		# usersDict['type'] = p['type']
+		# usersDict['tatMember'] = p['tatMember']
+		# if 'whichPositions' in p:
+		# 	usersDict['whichPositions'] = p['whichPositions']
+		# else:
+		# 	usersDict['whichPositions'] = "Not defined"
+		# usersList.append(usersDict)
 
 
 # Make that bigDict step by step
