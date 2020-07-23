c371f3bc1c4685a92eede01e852f2dca0a45a03a
805bluebell <805.bluebell@gmail.com>
2020-07-23 14:11:38 +0530

    just seeing if it could work

--- app.py
+++ app.py
@@ -1,52 +1,9 @@
-import flask
-from flask import request, jsonify, render_template, url_for
-from pymongo import MongoClient
-import json
-from bson import json_util, ObjectId
-from bson.int64 import Int64
-import datetime
+from flask import Flask, render_template
 
-app = flask.Flask(__name__,static_url_path='',
-            static_folder='static',
-            template_folder='templates')
-app.config["DEBUG"] = True
+app = Flask(__name__, static_url_path='',
+				  static_folder='static',
+				  template_folder='templates')
 
-client = MongoClient("mongodb://localhost:27017")
-database = client["local"]
-collection = database["antDB"]
-
-@app.route('/', methods=['GET'])
-def test1():
-	return render_template('test1.html')
-
-@app.route('/getTable', methods=['POST'])
-def getTable():
-	return "yes"
-
-if __name__ == '__main__':
-	app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Last
+@app.route("/")
+def hello():
+    return "Working fine"
