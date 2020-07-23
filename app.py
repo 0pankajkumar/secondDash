1a618f24c37e2791158446c5692906d9c1c85889
805bluebell <805.bluebell@gmail.com>
2020-07-23 14:19:04 +0530

    renamed it will fly now

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, static_url_path='',
-				  static_folder='static',
-				  template_folder='templates')
-
-@app.route("/")
-def hello():
-    return "Working fine"
