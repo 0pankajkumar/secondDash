c737b25d0a4d27d0c634e8d24848bf29e5846d8a
805bluebell <805.bluebell@gmail.com>
2020-07-28 16:35:20 +0530

    deleted unnecessary files

--- FlaskApp/static/dist/js/dropdownMultiselect.js
+++
@@ -1,9 +0,0 @@
-// $("#checkbox").click(function() {
-//   if ($("#checkbox").is(":checked")) {
-//     $("#e1 > option").prop("selected", "selected");
-//     $("#e1").trigger("change");
-//   } else {
-//     $("#e1 > option").removeAttr("selected");
-//     $("#e1").trigger("change");
-//   }
-// });
